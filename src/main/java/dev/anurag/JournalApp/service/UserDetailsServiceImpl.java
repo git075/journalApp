dc98b1eb66a82d5664d4e00ec4acaa5387f1d1c9
git075 <anuragchoudhary9929@gmail.com>
2025-11-15 21:13:36 +0530

    Minor fixes

--- src/main/java/dev/anurag/JournalApp/service/UserDetailsServiceImpl.java
+++ src/main/java/dev/anurag/JournalApp/service/UserDetailsServiceImpl.java
@@ -34,3 +34,40 @@ public class UserDetailsServiceImpl implements UserDetailsService{
         return null;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+We can access username with username without implementing userdetailsservice but spring security demands userdetails object and it does
+not understand out user entity. Also we have to check if the user is blocked or not or account expired because it is possible that
+it check the jwt token and it is not expired but user is blocked by the admin. So we do not completely rely on jwt token and we have to
+make db calls also to check the user status.
+* */
