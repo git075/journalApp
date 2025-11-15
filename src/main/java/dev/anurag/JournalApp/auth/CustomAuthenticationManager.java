862fd33f7cb697c0abc59dd222d2eac16a760a9d
git075 <anuragchoudhary9929@gmail.com>
2025-11-15 23:39:21 +0530

    Completed class CustomAuthenticationManager

--- src/main/java/dev/anurag/JournalApp/auth/CustomAuthenticationManager.java
+++ src/main/java/dev/anurag/JournalApp/auth/CustomAuthenticationManager.java
@@ -1,17 +1,61 @@
 package dev.anurag.JournalApp.auth;
 
+import dev.anurag.JournalApp.service.RedisLoginAttemptService;
+import dev.anurag.JournalApp.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CustomAuthenticationManager implements AuthenticationManager {
 
+    @Autowired
+    private RedisLoginAttemptService redisLoginAttemptService;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    private static final int MAX_ATTEMPTS = 5;
+
+
+
 
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        return null;
+        String username = authentication.getName();
+        String rawPassword = authentication.getCredentials().toString();
+
+        int attempts = redisLoginAttemptService.getAttempts(username);
+        if(attempts>=MAX_ATTEMPTS){
+            throw new LockedException("Account Blocked due to many attempts at once");
+        }
+        UserDetails user = userDetailsService.loadUserByUsername(username);
+
+        if(!passwordEncoder.matches(rawPassword, user.getPassword())){
+            redisLoginAttemptService.incrementAttempt(username);
+            throw new BadCredentialsException("Wrong Password or Username");
+        }
+
+        redisLoginAttemptService.resetAttempts(username);
+
+        return new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
     }
+
+    @Override
+    public boolean supports(Class<?> authentication){
+        return authentication.equals(UsernamePasswordAuthenticationFilter.class);
+    }
+
 }
