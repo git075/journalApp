d7587a61034712a6da9f6184d8c4c99303ba54e9
git075 <anuragchoudhary9929@gmail.com>
2025-11-16 22:08:20 +0530

    Added RedisLoginAttemptService

--- src/main/java/dev/anurag/JournalApp/service/RedisLoginAttemptService.java
+++ src/main/java/dev/anurag/JournalApp/service/RedisLoginAttemptService.java
@@ -1,7 +1,35 @@
 package dev.anurag.JournalApp.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class RedisLoginAttemptService {
+
+    private static final String ATTEMPT_PREFIX = "login_attempts";
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    public int getAttempts(String username){
+        String key = ATTEMPT_PREFIX + username;
+        String value = redisTemplate.opsForValue().get(key);
+        return value == null ? 0 : Integer.parseInt(value);
+    }
+
+    public void incrementAttempts(String username){
+        String key = ATTEMPT_PREFIX + username;
+        int attempts = getAttempts(username) + 1;
+        redisTemplate.opsForValue().set(key, String.valueOf(attempts), 15, TimeUnit.MINUTES);
+    }
+
+    public void resetAttempts(String username){
+        redisTemplate.delete(ATTEMPT_PREFIX + username);
+    }
+
+
+
 }
